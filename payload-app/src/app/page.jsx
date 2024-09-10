3555fdeba6d480293965339e9d4fda89066b0311
ab-h-i-n <abhinshelly@gmail.com>
2024-09-10 21:18:21 +0530

    fix : something

--- payload-app/src/app/page.jsx
+++ payload-app/src/app/page.jsx
@@ -5,27 +5,15 @@ export default async function Home() {
   const response = await fetch(`http://localhost:4000/api/items`);
   const Items = await response.json();
 
-  const handleSubmit = async(FormData) => {
-    "use server"
-    const formObject = Object.fromEntries(FormData.entries());
-    
-    await fetch('http://localhost:4000/api/items')
-
-  } 
-
-
   return (
     <>
       <div>
         {Items?.docs.map((item) => (
-          <div key={item.id} >{item.itemName}</div>
+          <div key={item.id}>{item.itemName}</div>
         ))}
       </div>
 
-      <form action={handleSubmit}>
-        <input type="text" name="itemName" />
-        <button type="submit" >Add</button>
-      </form>
+      <AddItemForm />
     </>
   );
 }
